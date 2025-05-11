26227ca7d4b248c048cc8b90d18929e910899041
oliveiralaurao <laura.p.oliveira@aluno.senai.br>
2025-05-11 19:54:54 -0300

    Update airport3 (7).sql

--- airport3 (7).sql
+++ airport3 (7).sql
@@ -43,18 +43,8 @@ CREATE TABLE `aeronaves` (
 
 INSERT INTO `aeronaves` (`id_aeronave`, `modelo_aeronave`, `capacidade_aeronave`, `quantidade_fileiras`, `quantidade_assentos_por_fileira`, `id_companhia`) VALUES
 (1, 'Boeing 787-8', 200, 25, 8, 1),
-(2, 'Airbus A320', 180, 30, 6, 1),
-(3, 'Boeing 737-800', 160, 27, 6, 1),
-(4, 'Embraer E190', 110, 22, 5, 4),
-(5, 'Boeing 777-300ER', 396, 33, 12, 7),
-(6, 'Airbus A350', 300, 50, 6, 9),
-(7, 'Boeing 787-9', 296, 37, 8, 2),
-(8, 'Airbus A330', 277, 34, 8, 1),
-(9, 'Bombardier CRJ-900', 90, 18, 5, 6),
-(10, 'ATR 72-600', 70, 14, 5, 1),
-(11, 'Cessna Citation X', 12, 4, 3, 5),
-(13, 'Boeing 737', 6, 2, 3, 4),
-(14, '221', 45, 9, 5, 7);
+(2, 'Airbus A320', 180, 30, 6, 1);
+
 
 -- --------------------------------------------------------
 
@@ -92,24 +82,7 @@ CREATE TABLE `aeroportos` (
 
 INSERT INTO `aeroportos` (`id_aeroporto`, `nome_aeroporto`, `codigo_aeroporto`, `localizacao_aeroporto`) VALUES
 (1, 'Aeroporto Internacional de São Paulo', 'GRU', 'São Paulo, SP, Brasil'),
-(2, 'Aeroporto de São Paulo/Congonhas–Deputado Freitas Nobre', 'CGH, SBSP', 'São Paulo, SP, Brasil'),
-(3, 'Aeroporto Internacional Tom Jobim', 'GIG', 'Rio de Janeiro, RJ'),
-(4, 'Aeroporto Internacional de Brasília', 'BSB', 'Brasília, DF'),
-(5, 'Aeroporto Internacional de Confins', 'CNF', 'Belo Horizonte, MG'),
-(6, 'Aeroporto Internacional de Salvador', 'SSA', 'Salvador, BA'),
-(7, 'Aeroporto Internacional de Porto Alegre', 'POA', 'Porto Alegre, RS'),
-(8, 'Aeroporto Internacional de Recife', 'REC', 'Recife, PE'),
-(9, 'Aeroporto Internacional de Fortaleza', 'FOR', 'Fortaleza, CE'),
-(10, 'Aeroporto Internacional de Manaus', 'MAO', 'Manaus, AM'),
-(11, 'Aeroporto Internacional Tom Jobim', 'GIG', 'Rio de Janeiro, Brasil'),
-(12, 'Aeroporto Internacional de Brasília', 'BSB', 'Brasília, Brasil'),
-(13, 'Aeroporto Internacional de Los Angeles', 'LAX', 'Los Angeles, EUA'),
-(14, 'Aeroporto Internacional de Londres Heathrow', 'LHR', 'Londres, Reino Unido'),
-(15, 'Aeroporto Internacional de Tóquio', 'HND', 'Tóquio, Japão'),
-(16, 'Aeroporto Internacional de Frankfurt', 'FRA', 'Frankfurt, Alemanha'),
-(17, 'Aeroporto Internacional de Paris-Charles de Gaulle', 'CDG', 'Paris, França'),
-(18, 'Aeroporto Internacional de Dubai', 'DXB', 'Dubai, Emirados Árabes'),
-(19, 'Aeroporto Internacional de Sydney', 'SYD', 'Sydney, Austrália');
+(2, 'Aeroporto de São Paulo/Congonhas–Deputado Freitas Nobre', 'CGH, SBSP', 'São Paulo, SP, Brasil');
 
 -- --------------------------------------------------------
 
@@ -130,219 +103,9 @@ CREATE TABLE `assentos` (
 --
 
 INSERT INTO `assentos` (`id_assento`, `numero_assento`, `status_assento`, `id_voo`) VALUES
-(1, 'A1', 'Disponível', 5),
-(2, 'B1', 'Disponível', 5),
-(3, 'C1', 'Corredor', 5),
-(4, 'D1', 'Disponível', 5),
-(5, 'F1', 'Ocupado', 5),
-(6, 'H1', 'Corredor', 5),
-(7, 'J1', 'Disponível', 5),
-(8, 'K1', 'Disponível', 5),
-(9, 'A2', 'Disponível', 5),
-(10, 'B2', 'Disponível', 5),
-(11, 'C2', 'Corredor', 5),
-(12, 'D2', 'Disponível', 5),
-(13, 'F2', 'Disponível', 5),
-(14, 'H2', 'Corredor', 5),
-(15, 'J2', 'Disponível', 5),
-(16, 'K2', 'Disponível', 5),
-(17, 'A3', 'Disponível', 5),
-(18, 'B3', 'Disponível', 5),
-(19, 'C3', 'Corredor', 5),
-(20, 'D3', 'Disponível', 5),
-(21, 'F3', 'Disponível', 5),
-(22, 'H3', 'Corredor', 5),
-(23, 'J3', 'Disponível', 5),
-(24, 'K3', 'Disponível', 5),
-(25, 'A4', 'Disponível', 5),
-(26, 'B4', 'Disponível', 5),
-(27, 'C4', 'Corredor', 5),
-(28, 'D4', 'Disponível', 5),
-(29, 'F4', 'Disponível', 5),
-(30, 'H4', 'Corredor', 5),
-(31, 'J4', 'Disponível', 5),
-(32, 'K4', 'Disponível', 5),
-(33, 'A5', 'Disponível', 5),
-(34, 'B5', 'Disponível', 5),
-(35, 'C5', 'Corredor', 5),
-(36, 'D5', 'Disponível', 5),
-(37, 'F5', 'Disponível', 5),
-(38, 'H5', 'Corredor', 5),
-(39, 'J5', 'Disponível', 5),
-(40, 'K5', 'Disponível', 5),
-(41, 'A6', 'Disponível', 5),
-(42, 'B6', 'Disponível', 5),
-(43, 'C6', 'Corredor', 5),
-(44, 'D6', 'Disponível', 5),
-(45, 'F6', 'Disponível', 5),
-(46, 'H6', 'Corredor', 5),
-(47, 'J6', 'Disponível', 5),
-(48, 'K6', 'Disponível', 5),
-(49, 'A7', 'Disponível', 5),
-(50, 'B7', 'Disponível', 5),
-(51, 'C7', 'Corredor', 5),
-(52, 'D7', 'Disponível', 5),
-(53, 'F7', 'Disponível', 5),
-(54, 'H7', 'Corredor', 5),
-(55, 'J7', 'Disponível', 5),
-(56, 'K7', 'Disponível', 5),
-(57, 'A8', 'Disponível', 5),
-(58, 'B8', 'Disponível', 5),
-(59, 'C8', 'Corredor', 5),
-(60, 'D8', 'Disponível', 5),
-(61, 'F8', 'Disponível', 5),
-(62, 'H8', 'Corredor', 5),
-(63, 'J8', 'Disponível', 5),
-(64, 'K8', 'Disponível', 5),
-(65, 'A9', 'Disponível', 5),
-(66, 'B9', 'Disponível', 5),
-(67, 'C9', 'Corredor', 5),
-(68, 'D9', 'Disponível', 5),
-(69, 'F9', 'Disponível', 5),
-(70, 'H9', 'Corredor', 5),
-(71, 'J9', 'Disponível', 5),
-(72, 'K9', 'Disponível', 5),
-(73, 'A10', 'Disponível', 5),
-(74, 'B10', 'Disponível', 5),
-(75, 'C10', 'Corredor', 5),
-(76, 'D10', 'Disponível', 5),
-(77, 'F10', 'Disponível', 5),
-(78, 'H10', 'Corredor', 5),
-(79, 'J10', 'Disponível', 5),
-(80, 'K10', 'Disponível', 5),
-(81, 'A11', 'Disponível', 5),
-(82, 'B11', 'Disponível', 5),
-(83, 'C11', 'Corredor', 5),
-(84, 'D11', 'Disponível', 5),
-(85, 'F11', 'Disponível', 5),
-(86, 'H11', 'Corredor', 5),
-(87, 'J11', 'Disponível', 5),
-(88, 'K11', 'Disponível', 5),
-(89, 'A12', 'Disponível', 5),
-(90, 'B12', 'Disponível', 5),
-(91, 'C12', 'Corredor', 5),
-(92, 'D12', 'Disponível', 5),
-(93, 'F12', 'Disponível', 5),
-(94, 'H12', 'Corredor', 5),
-(95, 'J12', 'Disponível', 5),
-(96, 'K12', 'Disponível', 5),
-(97, 'A13', 'Disponível', 5),
-(98, 'B13', 'Disponível', 5),
-(99, 'C13', 'Corredor', 5),
-(100, 'D13', 'Disponível', 5),
-(101, 'F13', 'Disponível', 5),
-(102, 'H13', 'Corredor', 5),
-(103, 'J13', 'Disponível', 5),
-(104, 'K13', 'Disponível', 5),
-(105, 'A14', 'Disponível', 5),
-(106, 'B14', 'Disponível', 5),
-(107, 'C14', 'Corredor', 5),
-(108, 'D14', 'Disponível', 5),
-(109, 'F14', 'Disponível', 5),
-(110, 'H14', 'Corredor', 5),
-(111, 'J14', 'Disponível', 5),
-(112, 'K14', 'Disponível', 5),
-(113, 'A15', 'Disponível', 5),
-(114, 'B15', 'Disponível', 5),
-(115, 'C15', 'Corredor', 5),
-(116, 'D15', 'Disponível', 5),
-(117, 'F15', 'Disponível', 5),
-(118, 'H15', 'Corredor', 5),
-(119, 'J15', 'Disponível', 5),
-(120, 'K15', 'Disponível', 5),
-(121, 'A16', 'Disponível', 5),
-(122, 'B16', 'Disponível', 5),
-(123, 'C16', 'Corredor', 5),
-(124, 'D16', 'Disponível', 5),
-(125, 'F16', 'Disponível', 5),
-(126, 'H16', 'Corredor', 5),
-(127, 'J16', 'Disponível', 5),
-(128, 'K16', 'Disponível', 5),
-(129, 'A17', 'Disponível', 5),
-(130, 'B17', 'Disponível', 5),
-(131, 'C17', 'Corredor', 5),
-(132, 'D17', 'Disponível', 5),
-(133, 'F17', 'Disponível', 5),
-(134, 'H17', 'Corredor', 5),
-(135, 'J17', 'Disponível', 5),
-(136, 'K17', 'Disponível', 5),
-(137, 'A18', 'Disponível', 5),
-(138, 'B18', 'Disponível', 5),
-(139, 'C18', 'Corredor', 5),
-(140, 'D18', 'Disponível', 5),
-(141, 'F18', 'Disponível', 5),
-(142, 'H18', 'Corredor', 5),
-(143, 'J18', 'Disponível', 5),
-(144, 'K18', 'Disponível', 5),
-(145, 'A19', 'Disponível', 5),
-(146, 'B19', 'Disponível', 5),
-(147, 'C19', 'Corredor', 5),
-(148, 'D19', 'Disponível', 5),
-(149, 'F19', 'Disponível', 5),
-(150, 'H19', 'Corredor', 5),
-(151, 'J19', 'Disponível', 5),
-(152, 'K19', 'Disponível', 5),
-(153, 'A20', 'Disponível', 5),
-(154, 'B20', 'Disponível', 5),
-(155, 'C20', 'Corredor', 5),
-(156, 'D20', 'Disponível', 5),
-(157, 'F20', 'Disponível', 5),
-(158, 'H20', 'Corredor', 5),
-(159, 'J20', 'Disponível', 5),
-(160, 'K20', 'Disponível', 5),
-(161, 'A21', 'Disponível', 5),
-(162, 'B21', 'Disponível', 5),
-(163, 'C21', 'Corredor', 5),
-(164, 'D21', 'Disponível', 5),
-(165, 'F21', 'Disponível', 5),
-(166, 'H21', 'Corredor', 5),
-(167, 'J21', 'Disponível', 5),
-(168, 'K21', 'Disponível', 5),
-(169, 'A22', 'Disponível', 5),
-(170, 'B22', 'Disponível', 5),
-(171, 'C22', 'Corredor', 5),
-(172, 'D22', 'Disponível', 5),
-(173, 'F22', 'Disponível', 5),
-(174, 'H22', 'Corredor', 5),
-(175, 'J22', 'Disponível', 5),
-(176, 'K22', 'Disponível', 5),
-(177, 'A23', 'Disponível', 5),
-(178, 'B23', 'Disponível', 5),
-(179, 'C23', 'Corredor', 5),
-(180, 'D23', 'Disponível', 5),
-(181, 'F23', 'Disponível', 5),
-(182, 'H23', 'Corredor', 5),
-(183, 'J23', 'Disponível', 5),
-(184, 'K23', 'Disponível', 5),
-(185, 'A24', 'Disponível', 5),
-(186, 'B24', 'Disponível', 5),
-(187, 'C24', 'Corredor', 5),
-(188, 'D24', 'Disponível', 5),
-(189, 'F24', 'Disponível', 5),
-(190, 'H24', 'Corredor', 5),
-(191, 'J24', 'Disponível', 5),
-(192, 'K24', 'Disponível', 5),
-(193, 'A25', 'Disponível', 5),
-(194, 'B25', 'Disponível', 5),
-(195, 'C25', 'Corredor', 5),
-(196, 'D25', 'Disponível', 5),
-(197, 'F25', 'Disponível', 5),
-(198, 'H25', 'Corredor', 5),
-(199, 'J25', 'Disponível', 5),
-(200, 'K25', 'Disponível', 5),
-(278, 'A1', 'Disponível', 2),
-(279, 'B1', 'Disponível', 2),
-(280, 'C1', 'Corredor', 2),
-(281, 'A2', 'Disponível', 2),
-(282, 'B2', 'Disponível', 2),
-(283, 'C2', 'Corredor', 2),
-(284, 'A3', 'Disponível', 2),
-(285, 'B3', 'Disponível', 2),
-(286, 'C3', 'Corredor', 2),
-(287, 'A4', 'Disponível', 2),
-(288, 'B4', 'Disponível', 2),
-(289, 'C4', 'Corredor', 2),
-(293, 'B12', 'Disponível', 1);
+(1, 'A1', 'Disponível', 1),
+(2, 'B1', 'Disponível', 1);
+
 
 -- --------------------------------------------------------
 
@@ -363,32 +126,8 @@ CREATE TABLE `companhias` (
 
 INSERT INTO `companhias` (`id_companhia`, `nome_companhia`, `codigo_companhia`) VALUES
 (1, 'Gol Linhas Aéreas Inteligentes', 'G3'),
-(2, 'Latam Airlines', 'LA'),
-(3, 'Azul Linhas Aéreas', 'AD'),
-(4, 'American Airlines', 'AA'),
-(5, 'Delta Airlines', 'DL'),
-(6, 'United Airlines', 'UA'),
-(7, 'Air France', 'AF'),
-(8, 'Lufthansa', 'LH'),
-(9, 'Qatar Airways', 'QR'),
-(10, 'Emirates', 'EK'),
-(11, 'British Airways', 'BA');
+(2, 'Latam Airlines', 'LA');
 
--- --------------------------------------------------------
-
---
--- Estrutura para tabela `mensagem_contato`
---
-
-DROP TABLE IF EXISTS `mensagem_contato`;
-CREATE TABLE `mensagem_contato` (
-  `id_mensagem` int(11) NOT NULL,
-  `conteudo_mensagem` text NOT NULL,
-  `email_mensagem` varchar(255) NOT NULL,
-  `data_envio` datetime NOT NULL,
-  `status_mensagem` enum('Lida','Não lida','Respondida') NOT NULL,
-  `passageiros_id_passageiros` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -414,7 +153,7 @@ CREATE TABLE `passageiros` (
 --
 
 INSERT INTO `passageiros` (`id_passageiros`, `nome_passageiro`, `email_passageiro`, `senha_passageiro`, `telefone_passageiro`, `pais_passageiro`, `documento_passageiro`, `datanasc_passageiro`, `tipo_passageiro`) VALUES
-(71, 'Laura Pimenta de Oliveira', 'laura.p@hotmail.com', '$2y$10$7sVF5QNWVewdKbWEjrjh..0dzDsrFcUjpwTnHBB4Xj4O41vmNLn5K', '(18) 99777-8384', 'Brasil', '463.100.198-48', '2005-10-21', '1');
+(1, 'teste', 'teste@teste.com', 'teste', '(99) 99999-9999', 'teste', '999.999.999-99', '0000-00-00', '1');
 
 -- --------------------------------------------------------
 
@@ -434,20 +173,8 @@ CREATE TABLE `passagens` (
 --
 
 INSERT INTO `passagens` (`id_passagem`, `valor_passagem`, `assentos_id_assento`) VALUES
-(2, 400, 5),
-(3, 300, 1),
-(4, 15, 282),
-(5, 15, 283),
-(6, 15, 284),
-(7, 15, 285),
-(8, 5, 281),
-(9, 5, 282),
-(10, 2147483647, 279),
-(11, 12, 142),
-(14, 499, 278),
-(15, 48, 293),
-(16, 87998, 280),
-(17, 87998, 286);
+(2, 400, 2),
+(3, 300, 1);
 
 -- --------------------------------------------------------
 
@@ -467,16 +194,10 @@ CREATE TABLE `portoes` (
 --
 
 INSERT INTO `portoes` (`id_portao`, `numero_portao`, `id_terminal`) VALUES
-(1, '2', 1),
-(3, 'C3', 2),
-(4, 'D4', 2),
-(5, 'E5', 3),
-(6, 'F6', 3),
-(7, 'G7', 4),
-(8, 'H8', 4),
-(9, 'I9', 5),
-(10, 'J10', 5),
-(11, 'A1', 1);
+
+(1, 'D4', 1),
+(2, 'E5', 1),
+(3, 'F6', 1);
 
 -- --------------------------------------------------------
 
@@ -498,8 +219,8 @@ CREATE TABLE `reservas` (
 --
 
 INSERT INTO `reservas` (`id_reserva`, `status_reserva`, `id_passageiro`, `id_voo`, `passagens_id_passagem`) VALUES
-(46, 'Cancelada', 71, 5, 3),
-(47, 'Confirmada', 71, 5, 2);
+(1, 'Cancelada', 1, 1, 1),
+(2, 'Confirmada', 1, 1, 1);
 
 -- --------------------------------------------------------
 
@@ -522,8 +243,8 @@ CREATE TABLE `segmentovoo` (
 
 INSERT INTO `segmentovoo` (`id_segmento`, `id_voo`, `destino`, `hora_partida`, `hora_chegada`) VALUES
 (1, 1, 2, '2024-12-14 19:20:00', '2024-12-19 19:20:00'),
-(4, 54, 10, '2004-01-26 14:50:00', '2222-01-26 14:51:00'),
-(5, 7, 18, '2025-04-14 14:37:00', '2025-04-14 14:38:00');
+(1, 2, 1, '2004-01-26 14:50:00', '2222-01-26 14:51:00'),
+
 
 -- --------------------------------------------------------
 
@@ -543,19 +264,9 @@ CREATE TABLE `terminais` (
 --
 
 INSERT INTO `terminais` (`id_terminal`, `id_aeroporto`, `nome_terminal`) VALUES
-(1, 2, '14'),
-(2, 1, 'Terminal 2 - Aeroporto Internacional São Paulo'),
-(3, 2, 'Terminal 1 - Aeroporto Internacional Rio de Janeiro'),
-(4, 2, 'Terminal 2 - Aeroporto Internacional Rio de Janeiro'),
-(5, 3, 'Terminal 1 - Aeroporto Internacional Brasília'),
-(6, 4, 'Terminal 3'),
-(7, 4, 'Terminal 1 - Aeroporto Internacional Salvador'),
-(8, 4, 'Terminal 2 - Aeroporto Internacional Salvador'),
-(9, 5, 'Terminal 1 - Aeroporto Internacional Recife'),
-(10, 5, 'Terminal 2 - Aeroporto Internacional Recife'),
-(11, 1, 'Terminal 1 - Aeroporto Internacional São Paulo'),
-(13, 1, 'bgvfcd'),
-(20, 19, 'eu consigoo voarrr\r\n');
+
+(1, 1, 'Terminal 2 - Aeroporto Internacional São Paulo'),
+(2, 2, 'Terminal 1 - Aeroporto Internacional Rio de Janeiro');
 
 -- --------------------------------------------------------
 
@@ -583,22 +294,9 @@ CREATE TABLE `voos` (
 --
 
 INSERT INTO `voos` (`id_voo`, `numero_voo`, `id_aeronave`, `id_portao`, `data_chegada`, `data_saida`, `origem_voo`, `destino_voo`, `tipo_voo`, `status_voo`, `localizacao_voo`) VALUES
-(1, 'AA1234', 1, 1, '2024-12-30 14:30:00', '2024-12-30 16:45:00', 1, 14, 'Escala', 'Reprogramado', 'Internacional'),
-(2, '744', 11, 1, '2024-12-12 04:15:00', '2024-12-17 05:15:00', 1, 19, 'Direto', 'Finalizado', 'Internacional'),
-(3, 'AA9012', 3, 3, '2024-12-24 10:00:00', '2024-12-24 07:00:00', 3, 4, 'Direto', 'Decolando', 'Nacional'),
-(4, 'DL3456', 4, 4, '2024-12-25 17:00:00', '2024-12-25 14:00:00', 4, 5, 'Escala', 'Cancelado', 'Nacional'),
-(5, '7445', 1, 1, '2024-12-27 18:40:00', '2024-12-12 19:40:00', 2, 8, 'Direto', 'Em Rota', 'Nacional'),
-(6, 'AF1234', 6, 6, '2024-12-27 14:00:00', '2024-12-27 11:00:00', 6, 7, 'Escala', 'Pousando', 'Nacional'),
-(7, 'LH5678', 7, 7, '2024-12-28 15:45:00', '2024-12-28 12:45:00', 7, 8, 'Direto', 'Atrasado', 'Nacional'),
-(8, 'QR9012', 8, 8, '2024-12-29 16:00:00', '2024-12-29 13:00:00', 8, 9, 'Escala', 'Reprogramado', 'Nacional'),
-(9, 'EK3456', 9, 9, '2024-12-30 13:30:00', '2024-12-30 10:30:00', 9, 10, 'Direto', 'Finalizado', 'Nacional'),
-(10, 'BA7890', 10, 10, '2024-12-31 12:00:00', '2024-12-31 09:00:00', 10, 11, 'Escala', 'Suspenso', 'Nacional'),
-(11, 'LA1234', 1, 1, '2024-12-22 11:00:00', '2024-12-22 08:00:00', 5, 2, 'Direto', 'No horário', 'Nacional'),
-(51, 'UA7890', 5, 5, '2024-12-26 19:30:00', '2024-12-26 16:30:00', 5, 6, 'Direto', 'Taxiando', 'Nacional'),
-(52, '744', 8, 9, '2000-04-17 11:07:00', '2025-04-17 11:56:00', 17, 18, 'Direto', 'Suspenso', 'Nacional'),
-(54, 'lalalalalalal', 13, 9, '1998-04-17 16:07:00', '2025-04-17 14:07:00', 14, 6, 'Direto', 'Desviado', 'Nacional'),
-(57, '48', 10, 7, '2025-04-21 19:42:00', '2025-04-21 20:42:00', 17, 14, 'Direto', 'Desviado', 'Nacional'),
-(59, 'gg', 14, 3, '2025-05-12 00:07:52', '2025-05-12 00:07:52', 15, 15, 'Direto', 'Em Rota', 'Nacional');
+(1, 'AA1234', 2, 1, '2024-12-30 14:30:00', '2024-12-30 16:45:00', 1, 2, 'Escala', 'Reprogramado', 'Internacional'),
+(2, 'AA9012', 1, 2, '2024-12-12 04:15:00', '2024-12-17 05:15:00', 2, 1, 'Direto', 'Finalizado', 'Nacional');
+
 
 -- --------------------------------------------------------
 
@@ -641,13 +339,7 @@ ALTER TABLE `companhias`
   ADD PRIMARY KEY (`id_companhia`);
 
 --
--- Índices de tabela `mensagem_contato`
---
-ALTER TABLE `mensagem_contato`
-  ADD PRIMARY KEY (`id_mensagem`),
-  ADD KEY `fk_mensagem_contato_passageiros1_idx` (`passageiros_id_passageiros`);
 
---
 -- Índices de tabela `passageiros`
 --
 ALTER TABLE `passageiros`
@@ -730,12 +422,6 @@ ALTER TABLE `companhias`
   MODIFY `id_companhia` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
--- AUTO_INCREMENT de tabela `mensagem_contato`
---
-ALTER TABLE `mensagem_contato`
-  MODIFY `id_mensagem` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
-
---
 -- AUTO_INCREMENT de tabela `passageiros`
 --
 ALTER TABLE `passageiros`
@@ -794,10 +480,6 @@ ALTER TABLE `assentos`
   ADD CONSTRAINT `assentos_ibfk_1` FOREIGN KEY (`id_voo`) REFERENCES `voos` (`id_voo`);
 
 --
--- Restrições para tabelas `mensagem_contato`
---
-ALTER TABLE `mensagem_contato`
-  ADD CONSTRAINT `fk_mensagem_contato_passageiros1` FOREIGN KEY (`passageiros_id_passageiros`) REFERENCES `passageiros` (`id_passageiros`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 --
 -- Restrições para tabelas `passagens`
